f7b61ec1fed0ae1ab442b9a765a23767e503caae
Lucas Tales <lucastales100@gmail.com>
2026-03-17 10:23:38 -0300

    feat: Add validation for negative triangle side lengths

--- triangulo.py
+++ triangulo.py
@@ -2,7 +2,9 @@ v1 = int(input("valor 1: "))
 v2 = int(input("valor 2: "))
 v3 = int(input("valor 3: "))
 
-if (v1 == v2 and v2 == v3 and (v1 + v2 > v3 and v2 + v3 > v1 and v3 + v1 > v2)):
+if (v1 < 0 or v2 < 0 or v3 < 0):
+    print("Valores inválidos")
+elif (v1 == v2 and v2 == v3 and (v1 + v2 > v3 and v2 + v3 > v1 and v3 + v1 > v2)):
     print("O triângulo é equilátero")
 elif (v1 + v2 > v3 and v2 + v3 > v1 and v3 + v1 > v2  and (v1 == v2 or v2 == v3 or v1 == v3)):
     print("O triângulo é isósceles")
